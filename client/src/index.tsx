373bc683144fea6635ceff01bc0a8b6c7cb060bc
Eduardo Sodré <90865245+YellowMaxSilver@users.noreply.github.com>
2025-04-29 04:09:53 -0300

    react update

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {superFacaReact} from './script';
 
 const myFirst = <h1>Hello World</h1>;
+
+const divStyle = {
+  border: "2px solid green",
+  backgroundColor:"gray",
+  display:"flex",
+  justifyContent:"center",
+  boxShadow:"0px 0px 10px black"
+};
+
+superFacaReact();
+
+const primitiveDiv = <div style={divStyle}>{myFirst}</div>;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(myFirst);
+root.render(primitiveDiv);
 // root.render(
 //   <React.StrictMode>
 //     <App />
--- public/scripts/client/src/index.js
+++ public/scripts/client/src/index.js
@@ -6,10 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const client_1 = __importDefault(require("react-dom/client"));
 require("./index.css");
+//import App from './App';
 const reportWebVitals_1 = __importDefault(require("./reportWebVitals"));
+const script_1 = require("./script");
 const myFirst = <h1>Hello World</h1>;
+const divStyle = {
+    border: "2px solid green",
+    backgroundColor: "gray",
+    display: "flex",
+    justifyContent: "center",
+    boxShadow: "0px 0px 10px black"
+};
+(0, script_1.superFacaReact)();
+const primitiveDiv = <div style={divStyle}>{myFirst}</div>;
 const root = client_1.default.createRoot(document.getElementById('root'));
-root.render(myFirst);
+root.render(primitiveDiv);
 // root.render(
 //   <React.StrictMode>
 //     <App />
